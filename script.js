998c130fdc967df378d6f967c8cc40f45c28c5c4
jasonsUX <jasonsux2004@gmail.com>
2026-04-28 17:10:58 -0700

    changes made

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 $(function(){
     $("#info").tabs();
+    fetchDogs();
 });
+
+let carouselInfo = document.getElementById("carousel");
+
+function fetchDogs(){
+    fetch("dogs.json")
+
+    // .then(response => {
+    //     if(!response.ok){
+    //         throw new Error("Network has received an error");
+    //     }
+    //     return response.json();
+    // })
+
+    .then(response => response.json())
+
+    // console.log(data)
+
+    .then(data =>{
+        console.log(data)
+    })
+}
+
+function getRandomDogs(){
+    
+}
+
+function displayDogs(){
+    let string = "";
+        for(let i = 0; i < 3; i++){
+            string += `
+            <section>
+                <img src="${data[i].image}" alt="${data[i].imgalt}">
+                <h2>${data[i].name}</h2>
+                <p>${data[i].desc}</p>            
+            </section>`
+        }
+        carouselInfo.innerHTML += string;
+}

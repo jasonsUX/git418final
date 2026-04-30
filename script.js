5b39468d56a11075724fc1fa7516a134fa949ecf
jasonsUX <jasonsux2004@gmail.com>
2026-04-30 01:20:49 -0700

    everything done

--- script.js
+++ script.js
@@ -61,5 +61,62 @@ function displayDogs(){
             </section>`
         }
         carouselInfo.innerHTML = string;
+    getCarousel();
 }
 
+function getCarousel(){
+    $("#carousel").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true
+    });
+}
+
+let lightDrk = document.getElementById("lightDark");
+let docBody = document.body;  
+let docHTML = document.querySelector("html");
+let info = document.getElementById("info")
+let h1 = document.querySelector("h1 > a");
+let nav = document.querySelectorAll("nav li > a");
+
+lightDrk.addEventListener("click", function(){
+        docBody.classList.toggle("invert");
+        docHTML.classList.toggle("dark");
+        info.classList.toggle("dark");
+        h1.classList.toggle("dark");
+        nav.forEach(item => {
+            item.classList.toggle("dark");
+        });
+    
+        let dark = docHTML.classList.contains("dark");
+
+        if(!dark){
+            localStorage.setItem("mode", "light");
+        }else{
+            localStorage.setItem("mode", "dark");
+        }
+});
+
+document.addEventListener("DOMContentLoaded", function (){
+
+    let prefMode = localStorage.getItem("mode");
+
+    if(prefMode === "dark") {
+        docBody.classList.add("invert");
+        docHTML.classList.add("dark");
+        info.classList.add("dark");
+        h1.classList.add("dark");
+        nav.forEach(item => {
+            item.classList.add("dark");
+        });
+    }else{
+        docBody.classList.remove("invert");
+        docHTML.classList.remove("dark");
+        info.classList.remove("dark");
+        h1.classList.remove("dark");
+        nav.forEach(item => {
+            item.classList.remove("dark");
+        });
+    }
+});
